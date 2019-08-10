84884733cbc68bb5f19315ea39e0529a414a806d
Nirmal Rijal <nirmalrijal41@gmail.com>
2019-08-10 11:37:30 +0545

    Add category feature

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const ora = require("ora");
-const path = require("path");
-const yargs = require("yargs");
-const request = require("request");
-
-const imageDownloadUrl = "https://unsplash.it/3360/1890/?random";
+const fs = require('fs');
+const ora = require('ora');
+const path = require('path');
+const yargs = require('yargs');
+const request = require('request');
 
 const argv = yargs
   .options({
     p: {
-      alias: "path",
-      describe: "Path to save images",
+      alias: 'path',
+      describe: 'Path to save images',
       string: true
     },
     n: {
-      alias: "num",
-      describe: "Total number of images to download",
+      alias: 'num',
+      describe: 'Total number of images to download',
+      string: true
+    },
+    c: {
+      alias: 'category',
+      describe: 'Category keyword separated by comma',
       string: true
     }
   })
   .help()
-  .alias("help", "h").argv;
+  .alias('help', 'h').argv;
 
 let i = 1;
 const max = argv.num ? argv.num : 10;
-const savePath = argv.path ? argv.path : "unsplashes";
+const savePath = argv.path ? argv.path : 'unsplashes';
+
+let imageDownloadUrl = argv.category
+  ? `https://source.unsplash.com/featured/3360x1890?${argv.category}`
+  : 'https://source.unsplash.com/featured/3360x1890';
 
 if (!fs.existsSync(savePath)) {
   fs.mkdirSync(savePath);
@@ -39,7 +46,7 @@ function downloadIMG() {
     const spinner = ora(`Downloading image ${i}`).start();
     request(imageDownloadUrl)
       .pipe(fs.createWriteStream(fileName))
-      .on("close", function(err) {
+      .on('close', function(err) {
         if (err) {
           console.log(err);
         }
@@ -49,7 +56,7 @@ function downloadIMG() {
       });
   } catch (err) {
     console.log(err);
-    console.log("Error on downloading image");
+    console.log('Error on downloading image');
   }
 }
 
